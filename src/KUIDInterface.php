2c381f79d21bda59210895ba74e68774dcd4ec53
Bruno Clemente <bruno.canguru@hotmail.com>
2024-11-14 17:06:37 -0300

    Add equals to interface

--- src/KUIDInterface.php
+++ src/KUIDInterface.php
@@ -7,12 +7,10 @@ use Ramsey\Uuid\UuidInterface;
 interface KUIDInterface {
 
 	public static function fromUUID(UuidInterface $uuid): static;
-
 	public static function random();
 	public static function fromString(string $string): static;
-
 	public function toUUID(): UuidInterface;
 	public function toString(): string;
-
+	public function equals(self $other): bool;
 	public function __toString(): string;
 }
